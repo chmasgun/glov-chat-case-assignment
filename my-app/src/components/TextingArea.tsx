55d3040499816c4f14897ba38c287992bb64e5c5
Muratcan Aşgün <masgun13@ku.edu.tr>
2024-08-23 20:13:53 +0300

    suggests words on typing
    when user types at least two characters, it checks if any suggestion is available. If yes, they are printed with an absolute div to the top. When clicked, they replace the inputValue. After choosing a suggestion, I temporarily disable suggestions, as we just clicked (boolean suggestionJustSet)

--- my-app/src/components/TextingArea.tsx
+++ my-app/src/components/TextingArea.tsx
@@ -3,7 +3,7 @@ import styled from 'styled-components';
 
 type TextingAreaProps = {
     // Add your props here
-    setMessages: React.Dispatch<React.SetStateAction<[string, number][]>>;  
+    setMessages: React.Dispatch<React.SetStateAction<[string, number][]>>;
 
 };
 
@@ -11,13 +11,19 @@ type TextingAreaProps = {
 const TextingAreaDiv = styled.div`
     display:flex;
     height: 60px;
-    overflow:clip;
+  
     gap: 1rem;
     justify-content:center;
     
     border-radius:  1rem; 
     `;
-
+const TextingAreaDivExtended = styled.div`
+    flex:1;
+    display:flex;
+    flex-direction: column;
+    position: relative; 
+    
+`;
 // Actual text are, input
 const TextingInput = styled.input`
     height: 60px;
@@ -44,16 +50,34 @@ const SendMessage = styled.div`
     }
     `;
 
-const TextingArea: React.FC<TextingAreaProps> = ({ setMessages }) => {
-    const autoCompleteMessages = ["Hello", "How are you?", "Long time no see!", "How was your day?", "Bye!", "See you later!", "I'll get back to you.", "Call me."]
 
+const SuggestionList = styled.span`
+    position: absolute;
+    top:0%;
+    width:100%;
+    background-color: rgb(220 220 220);
+    list-style: none;
+    padding: 0;
+    transform: translateY(-100%);
+`;
+
+const SuggestionItem = styled.li<{ highlighted: boolean }>`
+    color: ${(props) => (props.highlighted ? 'gray' : 'black')};
+    cursor: pointer;
+    padding: 0.5rem;
+`;
+
+const TextingArea: React.FC<TextingAreaProps> = ({ setMessages }) => {
+    
+    const [suggestions, setSuggestions] = useState<string[]>(["Hello", "How are you?", "Long time no see!", "How was your day?", "Bye!", "See you later!", "I'll get back to you.", "Call me."]);
 
     const [inputValue, setInputValue] = useState<string>(''); // Initialize input value state as empty
-
+    const [suggestionJustSet, setSuggestionJustSet] = useState<boolean>(false)
 
     const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
         // Update input value state on keyboard press
-        setInputValue(event.target.value); 
+        setInputValue(event.target.value);
+        setSuggestionJustSet(false);
     };
 
     const handleMessageSend = () => {
@@ -69,19 +93,43 @@ const TextingArea: React.FC<TextingAreaProps> = ({ setMessages }) => {
             handleMessageSend();
         }
     };
+    const handleSuggestionClick = (suggestion: string) => {
+        setInputValue(suggestion);
+        setSuggestionJustSet(true);
+    };
 
+    const filteredSuggestions = suggestions.filter((suggestion) =>
+         (!suggestionJustSet && // if suggestion is just clicked, we should not show it again
+            inputValue.length>1 && // if the user typed in at least two characters, suggestions might appear
+            suggestion.toLowerCase().includes(inputValue.toLowerCase())) 
+    );
 
     return (
 
         <TextingAreaDiv>
-            {/* input area, also watches for Enter press */}
-            <TextingInput
-                type="text"
-                value={inputValue}
-                onChange={handleInputChange}
-                onKeyDown={handleKeyPress}
-                placeholder="Type your message..."
-            />
+            {/* input zone with suggestions and combo box */}
+            <TextingAreaDivExtended>
+                {/* input area, also watches for Enter press */}
+                <TextingInput
+                    type="text"
+                    value={inputValue}
+                    onChange={handleInputChange}
+                    placeholder="Type your message..."
+                />
+                {filteredSuggestions.length > 0 && (
+                    <SuggestionList>
+                        {filteredSuggestions.map((suggestion, index) => (
+                            <SuggestionItem
+                                key={index}
+                                highlighted={suggestion.toLowerCase().startsWith(inputValue.toLowerCase())}
+                                onClick={() => handleSuggestionClick(suggestion)}
+                            >
+                                {suggestion}
+                            </SuggestionItem>
+                        ))}
+                    </SuggestionList>
+                )}
+            </TextingAreaDivExtended>
             {/* send button */}
             <SendMessage onClick={handleMessageSend}>Send</SendMessage>
         </TextingAreaDiv>
