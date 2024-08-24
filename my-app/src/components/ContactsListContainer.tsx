747a932f5344934131a16ad68fa8882adb781ab7
Muratcan Aşgün <masgun13@ku.edu.tr>
2024-08-24 11:08:30 +0300

    styles the contact div

--- my-app/src/components/ContactsListContainer.tsx
+++ my-app/src/components/ContactsListContainer.tsx
@@ -23,6 +23,23 @@ const ContactListDiv = styled.div`
     
  `;
 
+// Each individual contact card
+const ContactDiv = styled.div`
+    
+   margin: 0.5rem ;
+   background-color: rgb(203 213 235);
+   box-shadow: 0 1px 3px 0 rgb(0 0 0 / 0.1), 0 1px 2px -1px rgb(0 0 0 / 0.1);
+   border-bottom: 1px solid gray;
+   transition: border 0.3s,background-color 0.3s;
+   border-radius :   0.5rem 0.5rem 0 0;
+   cursor:pointer;
+    &:hover{
+        border-bottom: 1px solid limegreen;
+        background-color: rgb(208 223 240);
+        box-shadow: 0 10px 15px -3px rgb(0 0 0 / 0.1), 0 4px 6px -4px rgb(0 0 0 / 0.1);
+    }
+    
+ `;
 const ContactListContainer: React.FC<ContactListContainerProps> = ({ contactList, selectedContact, setSelectedContact }) => {
 
     
@@ -32,7 +49,7 @@ const ContactListContainer: React.FC<ContactListContainerProps> = ({ contactList
     return (
         <ContactListDiv>
             {
-                contactList.map((x,i) => <div key={i} style={{ height: "60px" , alignContent:"center" }} onClick={() => setSelectedContact(x)} >{x}</div>)
+                contactList.map((x,i) => <ContactDiv key={i} style={{ height: "60px" , alignContent:"center" }} onClick={() => setSelectedContact(x)} >{x}</ContactDiv>)
             }
         </ContactListDiv>
     );
